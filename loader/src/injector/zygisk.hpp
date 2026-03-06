e3169a09f1be0b69056c63bcffe590c2ddc2da4f
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-03-06 14:49:31 +0000

    refactor(injector): purge obsolete linker soinfo code (solist)

--- loader/src/injector/zygisk.hpp
+++ loader/src/injector/zygisk.hpp
@@ -22,9 +22,6 @@ void hook_entry(void *start_addr, size_t block_size);
 
 void hookJniNativeMethods(JNIEnv *env, const char *clz, JNINativeMethod *methods, int numMethods);
 
-void clean_linker_trace(const char *path, size_t loaded_modules, size_t unloaded_modules,
-                        bool unload_soinfo);
-
 void spoof_virtual_maps(const char *path, bool clear_write_permission);
 
 void spoof_zygote_fossil(char *search_from, char *search_to, const char *anchor);
