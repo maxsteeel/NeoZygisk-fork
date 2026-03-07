b2a83b4d12b1caedd3b37d62ba40ee08b15a0dba
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-03-07 02:24:05 +0000

    perf(files): replace heap allocations and malloc with a stack buffer in file readline

--- loader/src/common/files.cpp
+++ loader/src/common/files.cpp
@@ -1,24 +1,60 @@
 #include "files.hpp"
 
 #include <sys/sysmacros.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std::string_view_literals;
 
 void file_readline(bool trim, FILE *fp, const std::function<bool(std::string_view)> &fn) {
-    size_t len = 1024;
-    char *buf = (char *) malloc(len);
-    char *start;
-    ssize_t read;
-    while ((read = getline(&buf, &len, fp)) >= 0) {
-        start = buf;
-        if (trim) {
-            while (read && "\n\r "sv.find(buf[read - 1]) != std::string::npos) --read;
-            buf[read] = '\0';
-            while (*start == ' ') ++start;
+    if (!fp) return;
+
+    char buf[4096]; 
+
+    int fd = fileno(fp);
+    if (fd < 0) return;
+
+    ssize_t bytes_read;
+    size_t current_pos = 0;
+
+    while ((bytes_read = read(fd, buf + current_pos, sizeof(buf) - current_pos - 1)) > 0) {
+        size_t total_bytes = current_pos + bytes_read;
+        buf[total_bytes] = '\0';
+
+        char *line_start = buf;
+        char *line_end;
+
+        while ((line_end = strchr(line_start, '\n')) != nullptr) {
+            *line_end = '\0';
+
+            char *start = line_start;
+            size_t len = line_end - line_start;
+
+            if (trim) {
+                while (len > 0 && (start[len - 1] == '\r' || start[len - 1] == ' ')) {
+                    start[len - 1] = '\0';
+                    len--;
+                }
+
+                while (*start == ' ') {
+                    start++;
+                    len--;
+                }
+            }
+
+            if (!fn(std::string_view(start, len))) return;
+
+            line_start = line_end + 1;
         }
-        if (!fn(start)) break;
+
+        size_t remaining = total_bytes - (line_start - buf);
+        if (remaining > 0 && remaining < sizeof(buf)) {
+            memmove(buf, line_start, remaining);
+            current_pos = remaining;
+        } else {
+            current_pos = 0;
+        }
     }
-    free(buf);
 }
 
 void file_readline(bool trim, const char *file, const std::function<bool(std::string_view)> &fn) {
