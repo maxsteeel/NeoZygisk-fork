maxsteeel/NeoZygisk

refactor(zygiskd): Rewrite zygiskd entirely in native C++
This commit replaces the Rust implementation of the zygisk daemon (zygiskd) with a highly optimized, pure C++ implementation. This migration drastically reduces the binary footprint and overhead while maintaining full feature parity, stability, and compatibility.

Key Changes & Optimizations:
* Language Migration: Removed the entire Rust codebase (`zygiskd/src/*.rs`) and `Cargo.toml`, replacing it with a modular C++ architecture.
* Size Reduction: Halved the `zygiskd` binary size (from ~500 KB to ~244 KB). The total installed module size is now highly competitive (~793 KB), achieved by eliminating the heavy Rust standard library and avoiding `<iostream>` bloat.
* SELinux & IPC Integrity: Implemented precise SELinux context switching (`/proc/thread-self/attr/sockcreate`) to exactly mirror the original behavior, ensuring seamless Zygote connections and KernelSU/Magisk heartbeat pinging without `Permission denied` errors.
* Strict IPC Protocol: Rewrote Unix domain socket SCM_RIGHTS file descriptor passing (`send_fd`) to strictly transmit a 4-byte dummy payload, fixing desync issues and matching the `loader`'s `recv_fds` strict expectations.
* Robust Process Management: Implemented `SIGCHLD` ignoring to prevent Companion zombie processes, and `SIGPIPE` ignoring to protect the daemon from crashing during abrupt client disconnects.
* RAII Resource Management: Introduced `UniqueFile` and `UniquePipe` wrappers to guarantee safe file descriptor and pipe closures, preventing memory leaks during `popen` (`pclose`) and `fopen` operations.
* Direct SQLite Interfacing: Eliminated costly `popen("magisk --sqlite ...")` calls by dynamically loading Android's native `libsqlite.so` (`dlopen`) to directly query `magisk.db`, significantly improving startup performance when verifying UID capabilities.
* Namespace Handling: Preserved the critical `init` mount namespace jump (`switch_mount_namespace(1)`) while safely managing the CW

zygiskd/src/mount.cpp
@@ -1,0 +1,212 @@
+#include "mount.hpp"
+
+#include <fcntl.h>
+#include <sched.h>
+#include <sys/mount.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <vector>
+#include <string_view>
+#include <algorithm>
+
+#include "logging.hpp"
+#include "root_impl.hpp"
+#include "socket_utils.hpp"
+#include "misc.hpp"
+
+namespace zygisk_mount {
+
+bool switch_mount_namespace(pid_t pid) {
+    char cwd[4096];
+    if (getcwd(cwd, sizeof(cwd)) == nullptr) {
+        PLOGE("switch_mount_namespace: getcwd");
+        return false;
+    }
+
+    char ns_path[64];
+    snprintf(ns_path, sizeof(ns_path), "/proc/%d/ns/mnt", pid);
+
+    int fd = open(ns_path, O_RDONLY | O_CLOEXEC);
+    if (fd < 0) {
+        PLOGE("switch_mount_namespace: open %s", ns_path);
+        return false;
+    }
+
+    if (setns(fd, CLONE_NEWNS) != 0) {
+        PLOGE("switch_mount_namespace: setns");
+        close(fd);
+        return false;
+    }
+    close(fd);
+
+    if (chdir(cwd) != 0) {
+        PLOGE("switch_mount_namespace: chdir");
+        return false;
+    }
+
+    return true;
+}
+
+MountNamespaceManager::MountNamespaceManager() : clean_mnt_ns_fd_(-1), root_mnt_ns_fd_(-1) {}
+
+int MountNamespaceManager::get_namespace_fd(zygiskd::MountNamespace namespace_type) const {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return (namespace_type == zygiskd::MountNamespace::Clean) ? clean_mnt_ns_fd_ : root_mnt_ns_fd_;
+}
+
+int MountNamespaceManager::save_mount_namespace(pid_t pid, zygiskd::MountNamespace namespace_type) {
+    std::lock_guard<std::mutex> lock(mtx_);
+
+    int& fd_ref = (namespace_type == zygiskd::MountNamespace::Clean) ? clean_mnt_ns_fd_ : root_mnt_ns_fd_;
+    if (fd_ref >= 0) {
+        return fd_ref;
+    }
+
+    int pipefd[2];
+    if (pipe2(pipefd, O_CLOEXEC) != 0) {
+        PLOGE("pipe2");
+        return -1;
+    }
+
+    pid_t child_pid = fork();
+    if (child_pid < 0) {
+        PLOGE("fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
+    }
+
+    if (child_pid == 0) {
+        // --- Child Process ---
+        close(pipefd[0]);
+
+        if (!switch_mount_namespace(pid)) {
+            _exit(1);
+        }
+
+        if (namespace_type == zygiskd::MountNamespace::Clean) {
+            if (unshare(CLONE_NEWNS) != 0) {
+                PLOGE("unshare");
+                _exit(1);
+            }
+            if (!clean_mount_namespace()) {
+                LOGE("clean_mount_namespace failed");
+            }
+        }
+
+        uint8_t sig = 0;
+        if (socket_utils::xwrite(pipefd[1], &sig, sizeof(sig)) != sizeof(sig)) {
+            PLOGE("child: write pipe");
+        }
+        close(pipefd[1]);
+
+        while (true) {
+            sleep(60);
+        }
+        _exit(0);
+    }
+
+    // --- Parent Process ---
+    close(pipefd[1]);
+
+    uint8_t buf = 0;
+    if (socket_utils::xread(pipefd[0], &buf, sizeof(buf)) != sizeof(buf)) {
+        PLOGE("parent: read pipe");
+        kill(child_pid, SIGKILL);
+        waitpid(child_pid, nullptr, 0);
+        close(pipefd[0]);
+        return -1;
+    }
+    close(pipefd[0]);
+
+    char ns_path[64];
+    snprintf(ns_path, sizeof(ns_path), "/proc/%d/ns/mnt", child_pid);
+
+    int ns_fd = open(ns_path, O_RDONLY | O_CLOEXEC);
+    if (ns_fd < 0) {
+        PLOGE("open %s", ns_path);
+    }
+
+    kill(child_pid, SIGKILL);
+    waitpid(child_pid, nullptr, 0);
+
+    fd_ref = ns_fd;
+    return ns_fd;
+}
+
+struct MountInfo {
+    int mnt_id;
+    char path[256];
+};
+
+bool MountNamespaceManager::clean_mount_namespace() {
+    FILE* fp = fopen("/proc/self/mountinfo", "re");
+    if (!fp) [[unlikely]] {
+        PLOGE("fopen /proc/self/mountinfo");
+        return false;
+    }
+
+    std::vector<MountInfo> unmount_targets;
+    unmount_targets.reserve(256);
+
+    const char* root_source = nullptr;
+    auto root = root_impl::get();
+    if (root == root_impl::RootImpl::APatch) root_source = "APatch";
+    else if (root == root_impl::RootImpl::KernelSU) root_source = "KSU";
+    else if (root == root_impl::RootImpl::Magisk) root_source = "magisk";
+
+    bool is_ksu = (root == root_impl::RootImpl::KernelSU);
+    char ksu_module_source[256] = {0};
+
+    char line[1024];
+    while (fgets(line, sizeof(line), fp)) {
+        int mnt_id;
+        char root_path[256] = {0};
+        char mount_point[256] = {0};
+        char fstype[256] = {0};
+        char mount_source[256] = {0};
+
+        int ret = sscanf(line, "%d %*d %*s %255s %255s %*s %*s - %255s %255s",
+                         &mnt_id, root_path, mount_point, fstype, mount_source);
+
+        if (ret < 5) continue;
+
+        if (is_ksu && std::string_view(mount_point) == "/data/adb/modules") {
+            if (std::string_view(mount_source).starts_with("/dev/block/loop")) {
+                strlcpy(ksu_module_source, mount_source, sizeof(ksu_module_source));
+            }
+        }
+
+        bool should_unmount = false;
+        std::string_view root_view(root_path);
+        std::string_view mp_view(mount_point);
+        std::string_view src_view(mount_source);
+
+        if (root_view.starts_with("/adb/modules")) should_unmount = true;
+        else if (mp_view.starts_with("/data/adb/modules")) should_unmount = true;
+        else if (root_source && src_view == root_source) should_unmount = true;
+        else if (ksu_module_source[0] != '\0' && src_view == ksu_module_source) should_unmount = true;
+
+        if (should_unmount) {
+            MountInfo info;
+            info.mnt_id = mnt_id;
+            strlcpy(info.path, mount_point, sizeof(info.path));
+            unmount_targets.push_back(info);
+        }
+    }
+    fclose(fp);
+
+    std::sort(unmount_targets.begin(), unmount_targets.end(), [](const MountInfo& a, const MountInfo& b) {
+        return a.mnt_id > b.mnt_id;
+    });
+
+    for (const auto& target : unmount_targets) {
+        if (umount2(target.path, MNT_DETACH) == -1) {
+            PLOGE("umount2 %s", target.path);
+        }
+    }
+
+    return true;
+}
+
+} // namespace zygisk_mount

zygiskd/src/root_impl/apatch.cpp
@@ -1,0 +1,125 @@
+#include "apatch.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
+#include <mutex>
+
+#include "constants.hpp"
+#include "logging.hpp"
+
+namespace apatch {
+
+struct PackageInfo {
+    int32_t uid;
+    bool exclude;
+    bool allow;
+};
+
+static const char* CONFIG_FILE = "/data/adb/ap/package_config";
+
+static std::mutex cache_mutex;
+static std::optional<std::pair<time_t, std::vector<PackageInfo>>> config_cache;
+
+std::optional<Version> detect_version() {
+    FILE* fp = popen("apd -V", "r");
+    if (!fp) return std::nullopt;
+
+    char buf[128] = {0};
+    if (fgets(buf, sizeof(buf), fp)) {
+        char* token = strtok(buf, " \t\r\n");
+        if (token) {
+            token = strtok(nullptr, " \t\r\n");
+            if (token) {
+                int version = atoi(token);
+                if (fp) pclose(fp);
+                if (version >= MIN_APATCH_VERSION) {
+                    return Version::Supported;
+                } else {
+                    return Version::TooOld;
+                }
+            }
+        }
+    }
+    if (fp) pclose(fp);
+    return std::nullopt;
+}
+
+static std::optional<std::vector<PackageInfo>> get_config() {
+    struct stat st;
+    if (stat(CONFIG_FILE, &st) != 0) {
+        return std::nullopt;
+    }
+
+    time_t mtime = st.st_mtime;
+
+    {
+        std::lock_guard<std::mutex> lock(cache_mutex);
+        if (config_cache.has_value()) {
+            if (config_cache.value().first == mtime) {
+                return config_cache.value().second;
+            }
+        }
+    }
+
+    FILE* fp = fopen(CONFIG_FILE, "r");
+    if (!fp) return std::nullopt;
+
+    std::vector<PackageInfo> result;
+    char line[512];
+
+    // Skip header
+    if (!fgets(line, sizeof(line), fp)) {
+        fclose(fp);
+        return std::nullopt;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        int exclude_val, allow_val;
+        int32_t uid_val;
+
+        // format: pkg_name,exclude,allow,uid,...
+        char pkg_name[256];
+        if (sscanf(line, "%255[^,],%d,%d,%d", pkg_name, &exclude_val, &allow_val, &uid_val) >= 4) {
+            result.push_back({uid_val, exclude_val == 1, allow_val == 1});
+        }
+    }
+    fclose(fp);
+
+    {
+        std::lock_guard<std::mutex> lock(cache_mutex);
+        config_cache = std::make_pair(mtime, result);
+    }
+
+    return result;
+}
+
+bool uid_granted_root(int32_t uid) {
+    auto config = get_config();
+    if (!config.has_value()) return false;
+    for (const auto& pkg : config.value()) {
+        if (pkg.uid == uid && pkg.allow) return true;
+    }
+    return false;
+}
+
+bool uid_should_umount(int32_t uid) {
+    auto config = get_config();
+    if (!config.has_value()) return false;
+    for (const auto& pkg : config.value()) {
+        if (pkg.uid == uid && pkg.exclude) return true;
+    }
+    return false;
+}
+
+bool uid_is_manager(int32_t uid) {
+    struct stat st;
+    if (stat("/data/user_de/0/me.bmax.apatch", &st) == 0) {
+        return st.st_uid == static_cast<uid_t>(uid);
+    }
+    return false;
+}
+
+} // namespace apatch

maxsteeel/NeoZygisk-fork

zygiskd/src/mount.cpp
@@ -1,0 +1,212 @@
+#include "mount.hpp"
+
+#include <fcntl.h>
+#include <sched.h>
+#include <sys/mount.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <vector>
+#include <string_view>
+#include <algorithm>
+
+#include "logging.hpp"
+#include "root_impl.hpp"
+#include "socket_utils.hpp"
+#include "misc.hpp"
+#include "utils.hpp"
+
+namespace zygisk_mount {
+
+bool switch_mount_namespace(pid_t pid) {
+    char cwd[4096];
+    if (getcwd(cwd, sizeof(cwd)) == nullptr) {
+        PLOGE("switch_mount_namespace: getcwd");
+        return false;
+    }
+
+    char ns_path[64];
+    snprintf(ns_path, sizeof(ns_path), "/proc/%d/ns/mnt", pid);
+
+    int fd = open(ns_path, O_RDONLY | O_CLOEXEC);
+    if (fd < 0) {
+        PLOGE("switch_mount_namespace: open %s", ns_path);
+        return false;
+    }
+
+    if (setns(fd, CLONE_NEWNS) != 0) {
+        PLOGE("switch_mount_namespace: setns");
+        close(fd);
+        return false;
+    }
+    close(fd);
+
+    if (chdir(cwd) != 0) {
+        PLOGE("switch_mount_namespace: chdir");
+        return false;
+    }
+
+    return true;
+}
+
+MountNamespaceManager::MountNamespaceManager() : clean_mnt_ns_fd_(-1), root_mnt_ns_fd_(-1) {}
+
+int MountNamespaceManager::get_namespace_fd(zygiskd::MountNamespace namespace_type) const {
+    std::lock_guard<std::mutex> lock(mtx_);
+    return (namespace_type == zygiskd::MountNamespace::Clean) ? clean_mnt_ns_fd_ : root_mnt_ns_fd_;
+}
+
+int MountNamespaceManager::save_mount_namespace(pid_t pid, zygiskd::MountNamespace namespace_type) {
+    std::lock_guard<std::mutex> lock(mtx_);
+
+    int& fd_ref = (namespace_type == zygiskd::MountNamespace::Clean) ? clean_mnt_ns_fd_ : root_mnt_ns_fd_;
+    if (fd_ref >= 0) {
+        return fd_ref;
+    }
+
+    int pipefd[2];
+    if (pipe2(pipefd, O_CLOEXEC) != 0) {
+        PLOGE("pipe2");
+        return -1;
+    }
+
+    pid_t child_pid = fork();
+    if (child_pid < 0) {
+        PLOGE("fork");
+        close(pipefd[0]);
+        close(pipefd[1]);
+        return -1;
+    }
+
+    if (child_pid == 0) {
+        // --- Child Process ---
+        close(pipefd[0]);
+
+        if (!switch_mount_namespace(pid)) {
+            _exit(1);
+        }
+
+        if (namespace_type == zygiskd::MountNamespace::Clean) {
+            if (unshare(CLONE_NEWNS) != 0) {
+                PLOGE("unshare");
+                _exit(1);
+            }
+            if (!clean_mount_namespace()) {
+                LOGE("clean_mount_namespace failed");
+            }
+        }
+
+        uint8_t sig = 0;
+        if (socket_utils::xwrite(pipefd[1], &sig, sizeof(sig)) != sizeof(sig)) {
+            PLOGE("child: write pipe");
+        }
+        close(pipefd[1]);
+
+        while (true) {
+            sleep(60);
+        }
+        _exit(0);
+    }
+
+    // --- Parent Process ---
+    close(pipefd[1]);
+
+    uint8_t buf = 0;
+    if (socket_utils::xread(pipefd[0], &buf, sizeof(buf)) != sizeof(buf)) {
+        PLOGE("parent: read pipe");
+        kill(child_pid, SIGKILL);
+        waitpid(child_pid, nullptr, 0);
+        close(pipefd[0]);
+        return -1;
+    }
+    close(pipefd[0]);
+
+    char ns_path[64];
+    snprintf(ns_path, sizeof(ns_path), "/proc/%d/ns/mnt", child_pid);
+
+    int ns_fd = open(ns_path, O_RDONLY | O_CLOEXEC);
+    if (ns_fd < 0) {
+        PLOGE("open %s", ns_path);
+    }
+
+    kill(child_pid, SIGKILL);
+    waitpid(child_pid, nullptr, 0);
+
+    fd_ref = ns_fd;
+    return ns_fd;
+}
+
+struct MountInfo {
+    int mnt_id;
+    char path[256];
+};
+
+bool MountNamespaceManager::clean_mount_namespace() {
+    UniqueFile file(fopen("/proc/self/mountinfo",  "re"));
+    if (!file) [[unlikely]] {
+        PLOGE("fopen /proc/self/mountinfo");
+        return false;
+    }
+
+    std::vector<MountInfo> unmount_targets;
+    unmount_targets.reserve(256);
+
+    const char* root_source = nullptr;
+    auto root = root_impl::get();
+    if (root == root_impl::RootImpl::APatch) root_source = "APatch";
+    else if (root == root_impl::RootImpl::KernelSU) root_source = "KSU";
+    else if (root == root_impl::RootImpl::Magisk) root_source = "magisk";
+
+    bool is_ksu = (root == root_impl::RootImpl::KernelSU);
+    char ksu_module_source[256] = {0};
+
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        int mnt_id;
+        char root_path[256] = {0};
+        char mount_point[256] = {0};
+        char fstype[256] = {0};
+        char mount_source[256] = {0};
+
+        int ret = sscanf(line, "%d %*d %*s %255s %255s %*s %*s - %255s %255s",
+                         &mnt_id, root_path, mount_point, fstype, mount_source);
+
+        if (ret < 5) continue;
+
+        if (is_ksu && std::string_view(mount_point) == "/data/adb/modules") {
+            if (std::string_view(mount_source).starts_with("/dev/block/loop")) {
+                strlcpy(ksu_module_source, mount_source, sizeof(ksu_module_source));
+            }
+        }
+
+        bool should_unmount = false;
+        std::string_view root_view(root_path);
+        std::string_view mp_view(mount_point);
+        std::string_view src_view(mount_source);
+
+        if (root_view.starts_with("/adb/modules")) should_unmount = true;
+        else if (mp_view.starts_with("/data/adb/modules")) should_unmount = true;
+        else if (root_source && src_view == root_source) should_unmount = true;
+        else if (ksu_module_source[0] != '\0' && src_view == ksu_module_source) should_unmount = true;
+
+        if (should_unmount) {
+            MountInfo info;
+            info.mnt_id = mnt_id;
+            strlcpy(info.path, mount_point, sizeof(info.path));
+            unmount_targets.push_back(info);
+        }
+    }
+
+    std::sort(unmount_targets.begin(), unmount_targets.end(), [](const MountInfo& a, const MountInfo& b) {
+        return a.mnt_id > b.mnt_id;
+    });
+
+    for (const auto& target : unmount_targets) {
+        if (umount2(target.path, MNT_DETACH) == -1) {
+            PLOGE("umount2 %s", target.path);
+        }
+    }
+
+    return true;
+}
+
+} // namespace zygisk_mount

zygiskd/src/root_impl/apatch.cpp
@@ -1,0 +1,122 @@
+#include "apatch.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
+#include <mutex>
+
+#include "constants.hpp"
+#include "logging.hpp"
+#include "utils.hpp"
+
+namespace apatch {
+
+struct PackageInfo {
+    int32_t uid;
+    bool exclude;
+    bool allow;
+};
+
+static const char* CONFIG_FILE = "/data/adb/ap/package_config";
+
+static std::mutex cache_mutex;
+static std::optional<std::pair<time_t, std::vector<PackageInfo>>> config_cache;
+
+std::optional<Version> detect_version() {
+    UniquePipe pipe(popen("apd -V", "re"));
+    if (!pipe) return std::nullopt;
+
+    char buf[128] = {0};
+    if (fgets(buf, sizeof(buf), pipe)) {
+        char* token = strtok(buf, " \t\r\n");
+        if (token) {
+            token = strtok(nullptr, " \t\r\n");
+            if (token) {
+                int version = atoi(token);
+                if (version >= MIN_APATCH_VERSION) {
+                    return Version::Supported;
+                } else {
+                    return Version::TooOld;
+                }
+            }
+        }
+    }
+    return std::nullopt;
+}
+
+static std::optional<std::vector<PackageInfo>> get_config() {
+    struct stat st;
+    if (stat(CONFIG_FILE, &st) != 0) {
+        return std::nullopt;
+    }
+
+    time_t mtime = st.st_mtime;
+
+    {
+        std::lock_guard<std::mutex> lock(cache_mutex);
+        if (config_cache.has_value()) {
+            if (config_cache.value().first == mtime) {
+                return config_cache.value().second;
+            }
+        }
+    }
+
+    UniqueFile file(fopen(CONFIG_FILE, "re"));
+    if (!file) return std::nullopt;
+
+    std::vector<PackageInfo> result;
+    char line[512];
+
+    // Skip header
+    if (!fgets(line, sizeof(line), file)) {
+        return std::nullopt;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        int exclude_val, allow_val;
+        int32_t uid_val;
+
+        // format: pkg_name,exclude,allow,uid,...
+        char pkg_name[256];
+        if (sscanf(line, "%255[^,],%d,%d,%d", pkg_name, &exclude_val, &allow_val, &uid_val) >= 4) {
+            result.push_back({uid_val, exclude_val == 1, allow_val == 1});
+        }
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(cache_mutex);
+        config_cache = std::make_pair(mtime, result);
+    }
+
+    return result;
+}
+
+bool uid_granted_root(int32_t uid) {
+    auto config = get_config();
+    if (!config.has_value()) return false;
+    for (const auto& pkg : config.value()) {
+        if (pkg.uid == uid && pkg.allow) return true;
+    }
+    return false;
+}
+
+bool uid_should_umount(int32_t uid) {
+    auto config = get_config();
+    if (!config.has_value()) return false;
+    for (const auto& pkg : config.value()) {
+        if (pkg.uid == uid && pkg.exclude) return true;
+    }
+    return false;
+}
+
+bool uid_is_manager(int32_t uid) {
+    struct stat st;
+    if (stat("/data/user_de/0/me.bmax.apatch", &st) == 0) {
+        return st.st_uid == static_cast<uid_t>(uid);
+    }
+    return false;
+}
+
+} // namespace apatch