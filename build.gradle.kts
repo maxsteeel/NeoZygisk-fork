8a1587dea4b592dc2e5079f16edde69713f445e3
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-03-21 20:47:07 +0000

    experimental: update NDK to latest version r29

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ val androidCompileSdkVersion by extra(36)
 val androidBuildToolsVersion by extra("36.1.0")
 // Don't update NDK unless after careful and detailed tests,
 // as explained in https://github.com/JingMatrix/NeoZygisk/pull/36
-val androidCompileNdkVersion by extra("27.2.12479018")
+val androidCompileNdkVersion by extra("29.0.14206865")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_21)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_21)
 
